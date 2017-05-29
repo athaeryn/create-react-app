170b68f5b49d347eaddb3a7760d39a56333ba687
Mike Anderson <mike@mkndrsn.com>
2017-05-29 14:10:25 -0500

    Force elm loader watch mode

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -239,7 +239,7 @@ module.exports = {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         use: {
-          loader: 'elm-webpack-loader',
+          loader: 'elm-webpack-loader?forceWatch=true',
           options: {
             cwd: paths.elmSrc
           }
